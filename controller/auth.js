9fb18309560e64b6e0ea9c647bcd0b8f369f42df
Fadhlan Syahran Jungjungan <fadhlan.areksmp@gmail.com>
2023-12-28 20:17:15 +0700

    Adding JWT

--- controller/auth.js
+++ controller/auth.js
@@ -1,16 +1,16 @@
 import db from '../database/database.js'
 import bcrypt from 'bcryptjs'
-import { jwt } from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 
 export const register = (req, res) => {
     //Check Users Existing
-    const query = "SELECT * FROM users WHERE email = ? username = ?";
+    const q = "SELECT * FROM users WHERE email = ? OR username = ?";
     db.query(q, [req.body.email, req.body.name], (err, data) => {
-        if (err) return res.json(err)
+        if (err) return res.status(500).json(err)
         if (data.lenght) return res.status(409).json("User Already exist!");
         //Hash the password
 
-        const salt = bcrypt.getSaltSync(10);
+        const salt = bcrypt.genSaltSync(10);
         const hash = bcrypt.hashSync(req.body.password, salt);
 
         const q = "INSERT INTO users(`username`,`email`,`password`) VALUES (?)";
@@ -29,10 +29,10 @@ export const register = (req, res) => {
 export const login = (req, res) => {
     //Check User
 
-    const q = 'SELECT * FROM users WHERE users = ?'
+    const q = 'SELECT * FROM users WHERE username = ?'
     db.query(q, [req.body.username], (err, data) => {
-        if (err) return res.json(err);
-        if (data.lenght === 0) return res.status(404).json("User Not Found!")
+        if (err) return res.status(500).json(err);
+        if (data.length === 0) return res.status(404).json("User Not Found!")
 
         //Check Password
         const isPasswordCorrect = bcrypt.compareSync(req.body.password, data[0].password);
@@ -45,9 +45,10 @@ export const login = (req, res) => {
             httpOnly: true
         }).status(200).json(other)
     })
-
-
 }
 export const logout = (req, res) => {
-
+    res.clearCookie("access_token", {
+        sameSite: "none",
+        secure: true,
+    }).status(200).json("Berhasil melakukan Logout!");
 }
